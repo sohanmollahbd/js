603357e54f11bd791710afa0a4193a453f6bae65
sohanmollahbd <sohanmollahbd@gmail.com>
2024-08-30 17:46:05 +0600

    JAVASCRIPT

---
+++ js-conditionals/variables.js
@@ -0,0 +1,32 @@
+// Variable ==> container 
+var sunglassPrice = 80;
+
+// String 
+var subject = "Management";
+
+// Boolean 
+var passed = "true"
+
+/**
+ * Variable naming convention
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. cannot start with number
+ * 5. cannot be a keyword
+ * 6. prefer camelCase
+ */
+
+// camelCase
+var lastExamResult = 89;
+
+/**
+ * Operation
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. is NaN ?
+ * 4.Null: Null means having no value; in other words null is zero, like if you put so little sugar in your coffee that it's practically null. Null also means invalid, or having no binding force.
+ * 
+ * 5. number
+ * 
+ */
