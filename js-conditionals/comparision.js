8c132f1fba83eb2e114e8524fe04d5dad6ed4c61
sohanmollahbd <sohanmollahbd@gmail.com>
2024-08-30 20:13:41 +0600

    added comparision file

---
+++ js-conditionals/comparision.js
@@ -0,0 +1,18 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 >= 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(5 <= 3);
+
+
+// console.log(10 == 10);
+// console.log(5 == "5");
+// console.log(5 === "5"); //just value na type o dekhbe
+
+//----------
+
+console.log(10 != '10'); //check type , osoman kina?
+console.log(10 !== '10');// check value, osoman kina?
