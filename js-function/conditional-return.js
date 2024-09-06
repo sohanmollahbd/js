8ba9648c0a358ff3ee68d3ef6ce029241374e999
sohanmollahbd <sohanmollahbd@gmail.com>
2024-09-06 22:14:54 +0600

    added 2 new files related to js-function

---
+++ js-function/conditional-return.js
@@ -0,0 +1,20 @@
+function isEven (number){
+   if(number % 2 === 0){
+      return ture;
+   }
+   else {
+      return false;
+   }
+}
+console.log(isEven(3));
+
+
+function isOdd ( num2){
+   if(num2 % 2 === 1){
+      return true;
+   }
+   else{
+      return false;
+   }
+}
+console.log(isOdd(4));
