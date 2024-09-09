a240cc9d8fc5abe36799eee2647359ec3caae422
sohanmollahbd <sohanmollahbd@gmail.com>
2024-09-09 17:49:49 +0600

    added js-problems-part-1 files

--- js-problems-part-1/convert.js
+++ js-problems-part-1/convert.js
@@ -7,3 +7,41 @@ function inchToFeet (inch){
 
 const shuvoHeight = inchToFeet(75);
 console.log(shuvoHeight);
+
+function inchToFeet2 (inch){
+   const feet = inch / 12;
+   return feet;
+}
+
+const SohaHeight = inchToFeet2(80);
+console.log(SohaHeight);
+
+
+// ignore this one
+function inchToFeet2(inch) {
+   const feetFraction = inch / 12;
+   const feetNumber = parseInt(feetFraction);
+   const inchRemaining = inch % 12;
+   const result = feetNumber + ' ft ' + inchRemaining + ' inch.'
+   return result;
+}
+
+// const shuvoHeight = inchToFeet(75);
+// // console.log(shuvoHeight);
+const shuvoHeight2 = inchToFeet2(75);
+// console.log(shuvoHeight2)
+
+
+
+
+
+
+// mile to kilo meter 
+
+function mileToKiloMeter (mile){
+   const kiloMeter = mile * 1.609344;
+   return kiloMeter;
+}
+
+const Raj = mileToKiloMeter(3);
+console.log(Raj);
