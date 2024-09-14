3077a140a21e8596a43e2a675cdef3fe6cbd10c4
sohanmollahbd <sohanmollahbd@gmail.com>
2024-09-14 14:29:15 +0600

    added js-basics-bonus practice file

---
+++ js-basics-bonus/matched.js
@@ -0,0 +1,41 @@
+const numbers = [45, 65, 23, 98, 19];
+
+//  ekta ekta number ber kore niye asha 
+// for ( let i = 0 ; i < numbers.length ; i++){
+//    const number = numbers[i];
+//    console.log(number);
+// }
+
+// for (let number of numbers){
+//    console.log(number)
+// }
+
+const products = [
+   {id:1 , name: 'xiami phone One night', price: 19000},
+   {id:2 , name: 'iphone', price: 19000},
+   {id:3 , name: 'mac book air', price: 229000},
+   {id:4 , name: 'lenovo yoga laptop 2025', price: 19000},
+   {id:5 , name: 'Dell inspiron laptop', price: 19000},
+   {id:6 , name: 'Samsung phone note 7', price: 19000},
+   {id:7 , name: 'Nokia old age Phone gone', price: 19000},
+   {id:8 , name: 'Phone One night', price: 19000},
+   {id: 9, name: 'M1 chip not cheap LapTOP', price: 19000},
+
+];
+
+// for(const product of products){
+//    console.log(product)
+// }
+
+function matchedProducts(products, search){
+   const matched = []
+   for (const product of products){
+     if(product.name.toLowerCase().includes(search.toLowerCase())){
+      matched.push(product);
+     }
+   }
+   return matched;
+}
+
+const result = matchedProducts(products, 'laptop');
+console.log(result);
